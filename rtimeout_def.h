edfa4af19df376a5e08b21a0a4f1e650980d9323
Firsov Pavel Dmitrievich <p.d.firsov@gmail.com>
2024-04-09 02:25:10 +0300

    Fix defs and cmake

--- rtimeout_def.h
+++ rtimeout_def.h
@@ -8,7 +8,17 @@
 #    include "cmsis_os.h"
 #define get_tick() osKernelSysTick()
 #else
+#if defined(STM32F765xx)
+#include "stm32f7xx_hal.h"
+#elif defined(STM32G474xx)
+#include "stm32g4xx_hal.h"
+#elif defined(STM32F429xx) || defined(STM32F407xx)
+#include "stm32f4xx_hal.h"
+#elif defined(STM32F103xE)
 #include "stm32f1xx_hal.h"
+#elif defined(STM32F091xC) || defined(STM32F072xB)
+#include "stm32f0xx_hal.h"
+#endif
 #define get_tick() HAL_GetTick()
 #endif
 
