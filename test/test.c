9d441fd275feb8a45a235ad2cabecce90b3d6826
Firsov Pavel Dmitrievich <p.d.firsov@gmail.com>
2024-06-30 17:56:59 +0300

    More tests

--- test/test.c
+++ test/test.c
@@ -6,6 +6,11 @@ void test_timeout(void)
 {
     rt timeout = {0};
 
+    runit_true(rt_set(NULL, 100000) == -1);
+    runit_true(rt_set(&timeout, 0) == -1);
+    runit_true(rt_is_enabled(NULL) == -1);
+    runit_true(rt_timed_out(NULL) == -1);
+
     runit_true(rt_is_enabled(&timeout) == 0);
     runit_true(rt_set(&timeout, 1000000) == 0);
     runit_true(rt_is_enabled(&timeout) == 1);
