38fe36a39701db2432263cab3245346d180715e0
Firsov Pavel Dmitrievich <p.d.firsov@gmail.com>
2024-06-30 17:48:32 +0300

    Ref

--- rtimeout.h
+++ rtimeout.h
@@ -3,16 +3,27 @@
 
 #include "rtimeout_def.h"
 
+/** @brief set timeout for an instance
+ * @param instance - timeout the instance
+ * @return -1 if error
+ *          0 if ok
+ */
 int rt_set(rt* instance, unsigned long timeout_us);
 
 /** timed out?
- * @param instance - timeout instance
+ * @param instance - timeout the instance
  * @return -1 if error
  *          0 if timeout not elapsed
  *          1 already if timed out
  */
 int rt_timed_out(rt* instance);
 
+/** @brief the instance is enable
+ * @param instance - timeout the instance
+ * @return -1 if error
+ *          0 if timed out
+ *          1 if the instance is in process
+ */
 int rt_is_enabled(rt* instance);
 
 #endif
