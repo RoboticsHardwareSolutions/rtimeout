cd3fea1e1251cba8730b6ff4ebc9f5ddbfec4a5a
Sergey Golovin <golovinengineer@icloud.com>
2025-11-10 17:25:23 +0300

    Adds support for STM32G0B1XX microcontroller (#1)
    * feat: port to stm32g0b1xx
    
    * fix: typo in define

--- rtimeout.c
+++ rtimeout.c
@@ -1,91 +1,84 @@
 #include "rtimeout.h"
 #include "stdio.h"
 
-#if defined(STM32F072xB) || defined(STM32F091xC) || defined(STM32F103xB) || defined(STM32F407xx) || \
-    defined(STM32F429xx) || defined(STM32F103xE) || defined(STM32F765xx) || defined(STM32G474xx)
+#if defined(STM32F072xB) || defined(STM32F091xC) || defined(STM32F103xB) ||    \
+    defined(STM32F407xx) || defined(STM32F429xx) || defined(STM32F103xE) ||    \
+    defined(STM32F765xx) || defined(STM32G474xx) || defined(STM32G0B1xx)
 
-inline int rt_set(rt* instance, unsigned long timeout_us)
-{
-    if (instance == NULL || timeout_us == 0)
-    {
-        return -1;
-    }
-    instance->start_time = get_tick();
-    instance->timeout    = timeout_us / 1000;
-    return 0;
+inline int rt_set(rt *instance, unsigned long timeout_us) {
+  if (instance == NULL || timeout_us == 0) {
+    return -1;
+  }
+  instance->start_time = get_tick();
+  instance->timeout = timeout_us / 1000;
+  return 0;
 }
 
-inline int rt_timed_out(rt* instance)
-{
-    if (instance == NULL)
-        return -1;
+inline int rt_timed_out(rt *instance) {
+  if (instance == NULL)
+    return -1;
 
-    unsigned long current_time = get_tick();
-    unsigned long elapsed_time = current_time - instance->start_time;
-    int           result       = instance->timeout > 0 && elapsed_time > instance->timeout ? 1 : 0;
+  unsigned long current_time = get_tick();
+  unsigned long elapsed_time = current_time - instance->start_time;
+  int result =
+      instance->timeout > 0 && elapsed_time > instance->timeout ? 1 : 0;
 
-    if (result)
-    {
-        instance->timeout = 0;
-    }
-    return result;
+  if (result) {
+    instance->timeout = 0;
+  }
+  return result;
 }
 
-inline int rt_is_enabled(rt* instance)
-{
-    if (instance == NULL)
-        return -1;
+inline int rt_is_enabled(rt *instance) {
+  if (instance == NULL)
+    return -1;
 
-    return instance->timeout > 0 ? 1 : 0;
+  return instance->timeout > 0 ? 1 : 0;
 }
 
 #endif
 
-#if defined(RTIMEOUT_FOR_WINDOWS) || defined(RTIMEOUT_FOR_UNIX) || defined(RTIMEOUT_FOR_APPLE)
+#if defined(RTIMEOUT_FOR_WINDOWS) || defined(RTIMEOUT_FOR_UNIX) ||             \
+    defined(RTIMEOUT_FOR_APPLE)
 
-int rt_set(rt* instance, unsigned long timeout_us)
-{
-    if (instance == NULL || timeout_us == 0)
-    {
-        return -1;
-    }
+int rt_set(rt *instance, unsigned long timeout_us) {
+  if (instance == NULL || timeout_us == 0) {
+    return -1;
+  }
 
-    if (gettimeofday(&instance->start_time, NULL) != 0)
-    {
-        return -1;
-    }
-    instance->timeout = timeout_us;
-    return 0;
+  if (gettimeofday(&instance->start_time, NULL) != 0) {
+    return -1;
+  }
+  instance->timeout = timeout_us;
+  return 0;
 }
 
-inline int rt_timed_out(rt* instance)
-{
-    if (instance == NULL)
-        return -1;
+inline int rt_timed_out(rt *instance) {
+  if (instance == NULL)
+    return -1;
 
-    struct timeval current_time;
-    if (gettimeofday(&current_time, NULL) != 0)
-    {
-        return -1;
-    }
-    uint32_t elapsed_time = (uint32_t) ((current_time.tv_sec - instance->start_time.tv_sec) * 1000000 +
-                                        current_time.tv_usec - instance->start_time.tv_usec);
+  struct timeval current_time;
+  if (gettimeofday(&current_time, NULL) != 0) {
+    return -1;
+  }
+  uint32_t elapsed_time =
+      (uint32_t)((current_time.tv_sec - instance->start_time.tv_sec) * 1000000 +
+                 current_time.tv_usec - instance->start_time.tv_usec);
 
-    int result = instance->timeout > 0 && elapsed_time > instance->timeout ? 1 : 0;
+  int result =
+      instance->timeout > 0 && elapsed_time > instance->timeout ? 1 : 0;
 
-    if (result)
-    {
-        instance->timeout = 0;
-    }
-    return result;
+  if (result) {
+    instance->timeout = 0;
+  }
+  return result;
 }
 
-inline int rt_is_enabled(rt* instance)
-{
-    if (instance == NULL)
-        return -1;
+inline int rt_is_enabled(rt *instance) {
+  if (instance == NULL)
+    return -1;
 
-    return instance->timeout > 0 ? 1 : 0;
+  return instance->timeout > 0 ? 1 : 0;
 }
 
 #endif
--- rtimeout_def.h
+++ rtimeout_def.h
@@ -1,47 +1,50 @@
 #pragma once
 
-#if defined(STM32G474xx) || defined(STM32F103xB) || defined(STM32F072xB) ||    \
-    defined(STM32F091xC) || defined(STM32F407xx) || defined(STM32F429xx) ||    \
-    defined(STM32F765xx) || defined(STM32F103xE)
+#if defined(STM32G474xx) || defined(STM32F103xB) || defined(STM32F072xB) || defined(STM32F091xC) || \
+    defined(STM32F407xx) || defined(STM32F429xx) || defined(STM32F765xx) || defined(STM32F103xE) || \
+    defined(STM32G0B1xx)
 
-#ifdef CMSIS_OS_H_
-#include "cmsis_os.h"
-#define get_tick() osKernelSysTick()
-#else
-#if defined(STM32F765xx)
-#include "stm32f7xx_hal.h"
-#elif defined(STM32G474xx)
-#include "stm32g4xx_hal.h"
-#elif defined(STM32F429xx) || defined(STM32F407xx)
-#include "stm32f4xx_hal.h"
-#elif defined(STM32F103xE)
-#include "stm32f1xx_hal.h"
-#elif defined(STM32F091xC) || defined(STM32F072xB)
-#include "stm32f0xx_hal.h"
-#endif
-#define get_tick() HAL_GetTick()
-#endif
+#    ifdef CMSIS_OS_H_
+#        include "cmsis_os.h"
+#        define get_tick() osKernelSysTick()
+#    else
+#        if defined(STM32F765xx)
+#            include "stm32f7xx_hal.h"
+#        elif defined(STM32G0B1xx)
+#            include "stm32g0xx_hal.h"
+#        elif defined(STM32G474xx)
+#            include "stm32g4xx_hal.h"
+#        elif defined(STM32F429xx) || defined(STM32F407xx)
+#            include "stm32f4xx_hal.h"
+#        elif defined(STM32F103xE)
+#            include "stm32f1xx_hal.h"
+#        elif defined(STM32F091xC) || defined(STM32F072xB)
+#            include "stm32f0xx_hal.h"
+#        endif
+#        define get_tick() HAL_GetTick()
+#    endif
 
-typedef struct {
-  unsigned long start_time;
-  unsigned long timeout;
+typedef struct
+{
+    unsigned long start_time;
+    unsigned long timeout;
 } rt;
 
 #endif
 
-#if defined(RTIMEOUT_FOR_APPLE) || defined(RTIMEOUT_FOR_UNIX) ||               \
-    defined(RTIMEOUT_FOR_WINDOWS)
+#if defined(RTIMEOUT_FOR_APPLE) || defined(RTIMEOUT_FOR_UNIX) || defined(RTIMEOUT_FOR_WINDOWS)
 
-#include "stdbool.h"
-#include "stdint.h"
-#include "stdio.h"
-#include <signal.h>
-#include <sys/time.h>
-#include <time.h>
+#    include "stdbool.h"
+#    include "stdint.h"
+#    include "stdio.h"
+#    include <signal.h>
+#    include <sys/time.h>
+#    include <time.h>
 
-typedef struct {
-  struct timeval start_time;
-  uint32_t timeout;
+typedef struct
+{
+    struct timeval start_time;
+    uint32_t       timeout;
 } rt;
 
 #endif
