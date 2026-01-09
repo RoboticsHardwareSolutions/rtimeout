491c65d0cc14316504e6588841071bbd916b1f99
Doc Pillman <doc.pillman@icloud.com>
2026-01-09 20:33:10 +0300

    add format section

--- rtimeout.h
+++ rtimeout.h
@@ -7,7 +7,7 @@
  * @return -1 if error
  *          0 if ok
  */
-int rt_set(rt *instance, unsigned long timeout_us);
+int rt_set(rt* instance, unsigned long timeout_us);
 
 /** timed out?
  * @param instance - timeout the instance
@@ -15,7 +15,7 @@ int rt_set(rt *instance, unsigned long timeout_us);
  *          0 if timeout not elapsed
  *          1 already if timed out
  */
-int rt_timed_out(rt *instance);
+int rt_timed_out(rt* instance);
 
 /** @brief the instance is enable
  * @param instance - timeout the instance
@@ -23,4 +23,4 @@ int rt_timed_out(rt *instance);
  *          0 if timed out
  *          1 if the instance is in process
  */
-int rt_is_enabled(rt *instance);
+int rt_is_enabled(rt* instance);
